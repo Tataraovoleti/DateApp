b51234d31271330700482a771315aaeb0c7c863e
Tatarao Voleti <tatarao.oleti@gmail.com>
2013-12-24 17:08:23 +0530

    Added timestamp value in query

--- src/dateapp/DateRetrieve.java
+++ src/dateapp/DateRetrieve.java
@@ -12,34 +12,37 @@ import java.text.SimpleDateFormat;
 
 /**
  * Technology : Java
- * Version      : 1.6
- * Database    : MySQL
+ * Version    : 1.6
+ * Database   : MySQL
  * @author TATARAO
  */
 public class DateRetrieve {
     public static void main(String arg[]) throws Exception
 	{
-		
 		Class.forName("org.gjt.mm.mysql.Driver");
 		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/rao","root","");
-
 	
-	    PreparedStatement ps = con.prepareStatement("select * from date_tab");
-
+	        PreparedStatement ps = con.prepareStatement("select * from date_tab");
 		ResultSet rs= ps.executeQuery();
-		
 		while(rs.next())
 		{
 		System.out.print(rs.getInt(1)+" "+rs.getString(2)+" ");
-
-         // converting java.sql.Date format to java.util.Date format
-
+                // converting java.sql.Date format to java.util.Date format
 		 java.sql.Date sqd= rs.getDate(3);
 		 java.util.Date ud= (java.util.Date)sqd;
-
-    System.out.println(" "+ud.toString());
+		 
+		  java.sql.Timestamp stmp= rs.getTimestamp(4);
+		  long ms= stmp.getTime();
+		  java.util.Date udd=new java.util.Date(ms);
+                
+                //System.out.println(" "+ud.toString());
+                /* Mysql stores date in the form of yyyy-MM-dd , timestamp in the form of yyyy-MM-dd HH:mm:ss
+                 * Now we are getting the date and timestamp form DB and parsing it into required pattern  
+                 * ie, dd/MMM/yyyy. we can change this format
+                */
 		SimpleDateFormat sfd= new SimpleDateFormat("dd/MMM/yyyy");
-          System.out.println(" "+sfd.format(ud));
+                System.out.println("Dob - "+sfd.format(ud));
+                System.out.println("Creation time - "+sfd.format(udd));
 		}
 		   
 	   ps.close();
