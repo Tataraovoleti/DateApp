6e64f5492153992922d7892b721d714541f08a58
Tatarao Voleti <tatarao.oleti@gmail.com>
2013-12-24 16:55:17 +0530

    Added timestamp date format...

--- src/dateapp/DateApp.java
+++ src/dateapp/DateApp.java
@@ -22,9 +22,8 @@ public class DateApp {
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         
-
 		Class.forName("org.gjt.mm.mysql.Driver");
-		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/rao","root","");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/rao","root","root");
 	PreparedStatement ps=null;
 		// Converting String date values to java.util.Date class obj
 		
@@ -35,10 +34,10 @@ public class DateApp {
 		System.out.println("enter student name : ");
 		String name= sc.next();
 
-        System.out.println("enter student dob(dd-MMM-YY) : ");
+               System.out.println("enter student dob(dd-MMM-yyyy) : ");
 		String dob= sc.next();
 
-		SimpleDateFormat sfd= new SimpleDateFormat("dd-MMM-yy");
+		SimpleDateFormat sfd= new SimpleDateFormat("dd-MMM-yyyy");
 		java.util.Date ud= sfd.parse(dob); 
 
 		System.out.println(" "+ud.toString());
@@ -46,29 +45,29 @@ public class DateApp {
        // Converting java.util.Date value to java.sql.Date class obj
 
 	   long ms = ud.getTime();
-
 	   java.sql.Date sqld= new java.sql.Date(ms); 
 
 //	   java.sql.Date sqld=java.sql.Date.valueOf(dob);
 	   System.out.println("sql date is "+sqld.toString());
 
-
 	   // create prepare statement obj
            try
            {
-
-	   ps = con.prepareStatement("insert into date_tab values(?,?,?)");
+           // our table date_tab contains 4 fields
+           // no int(10),name varchar(20), dob date, created_time datetime
+	   ps = con.prepareStatement("insert into date_tab values(?,?,?,?)");
 
 	   ps.setInt(1,no);
 	   ps.setString(2,name);
 	   ps.setDate(3,sqld);
+	   ps.setTimestamp(4,new Timestamp(new Date().getTime()));
 
 	   int res= ps.executeUpdate();
 
 	   if(res==0)
 		   System.out.println(" insertion problem");
 	   else
-	   System.out.println(" insertion successful");
+	         System.out.println(" insertion successful");
            }
            catch(Exception e)
            {
